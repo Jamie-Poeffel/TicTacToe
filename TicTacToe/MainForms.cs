7295255d90be75f2337e0b3a2507be13f51c1f91
Jamie Poeffel <Jamie_Poeffel@sluz.ch>
2024-05-01 21:58:11 +0200

    Resourcen Hinzu gefügt

--- TicTacToe/MainForms.cs
+++ TicTacToe/MainForms.cs
@@ -13,9 +13,42 @@ namespace TicTacToe
   public partial class MainForms : Form
   {
     WriteLogs Log = null;
+    Pieces[,] Board = new Pieces[3, 3];
+    int role = 0, xScore = 0, oScore = 0;
     public MainForms()
     {
       InitializeComponent();
+      Initial();
+    }
+
+    private void Initial()
+    {
+      for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+        {
+          Board[i, j] = new Pieces(j * 100, i * 100);
+          Board[i, j].Click += Play;
+          Board[i, j].Cursor = Cursors.Hand;
+          this.Controls.Add(Board[i, j]);
+        }
+    }
+
+    private void Play(object sender, EventArgs e)
+    {
+      int i  = ((Button)sender).Top / 100, j = ((Button)sender).Left / 100;
+      if (Board[i, j].state == States.F)
+      {
+        if (role % 2 == 0)
+        {
+          Board[i, j].state = States.X;
+          Board[i, j].Image = Properties.Resources.X;
+        }else 
+        {
+          Board[i, j].state = States.O;
+          Board[i, j].Image = Properties.Resources.O;
+        }
+        role += 1;
+      }else MessageBox.Show("Invalid place");
     }
 
     private void OnFormsLoad(object sender, EventArgs e)
---
+++ TicTacToe/Pieces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+  enum States
+  {
+    X,
+    O,
+    F
+  }
+  class Pieces : Button
+  {
+    public States state = States.F;
+    public Pieces(int x, int y)
+    {
+      Location = new System.Drawing.Point(x, y);
+      Size = new System.Drawing.Size(100, 100);
+    }
+  }
+}
